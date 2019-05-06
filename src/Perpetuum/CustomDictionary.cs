c868497d8098a4723c601e5807d6ff9c58b03475
Mark Rutherford <mark@lowcountrybilling.com>
2019-05-06 13:14:22 -0400

    Custom dictionary to single instance. basic sanity checks..

--- src/Perpetuum/CustomDictionary.cs
+++ src/Perpetuum/CustomDictionary.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Perpetuum.Log;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,20 +15,52 @@ namespace Perpetuum
 
     public class CustomDictionary : ICustomDictionary
     {
-        private readonly int _defaultLanguage = 0;
+        /*
+        Language IDs:
+            0 = English
+            1 = Hungarian
+            2 = German
+            3 = Portuguese
+            4 = Russian
+            5 = French
+            6 = Spanish
+            7 = Polish
+            8 = Slovenian
+            9 = Romanian
+            10 = Norwegian
+            11 = Greek
+            12 = Finnish
+            13 = Italian
+            14 = Turkish
+            15 = Estonian
+            16 = Swedish
+            17 = Dutch
+        */
 
+        private readonly int _defaultLanguage = 0;
         public Dictionary<int, Dictionary<string, object>> _dictionaries;
-        public CustomDictionary(Perpetuum.IO.IFileSystem fileManager)
+
+        public CustomDictionary(IO.IFileSystem fileManager)
         {
             _dictionaries = new Dictionary<int, Dictionary<string, object>>();
-            var files = fileManager.GetFiles("customDictionary", "*.json");
 
-            foreach (var file in files)
+            // if the directory doesn't exist or the JSON is malformed just throw an exception, log it and move on.
+            // Otherwise we get a blank screen on the client.
+            try
+            {                
+                var files = fileManager.GetFiles("customDictionary", "*.json");
+
+                foreach (var file in files)
+                {
+                    var languageID = Convert.ToInt32(System.IO.Path.GetFileNameWithoutExtension(file));
+                    var settingsFile = fileManager.ReadAllText(file);
+                    var dictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(settingsFile);
+                    _dictionaries.Add(languageID, dictionary);
+                }
+            }
+            catch(Exception ex)
             {
-                var languageID = Convert.ToInt32(System.IO.Path.GetFileNameWithoutExtension(file));
-                var settingsFile = fileManager.ReadAllText(file);
-                var dictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(settingsFile);
-                _dictionaries.Add(languageID, dictionary);
+                Logger.Error(ex.Message);
             }
         }
 
@@ -37,7 +70,7 @@ namespace Perpetuum
             {
                 return _dictionaries[language];
             }
-            if (_dictionaries.ContainsKey(_defaultLanguage))
+            else if (_dictionaries.ContainsKey(_defaultLanguage))
             {
                 return _dictionaries[_defaultLanguage];
             }
