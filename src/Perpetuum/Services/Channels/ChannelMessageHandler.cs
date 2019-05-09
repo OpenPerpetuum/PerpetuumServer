003655eb63a5d4590efb695d3a4843a09c4516dd
Philippe Cloutier <philippe.cloutier.1@gmail.com>
2019-05-08 22:52:27 -0400

    - Update the IChannelManager interface to support announcement using character that are from the system (not logged in) - Update the Channel Message Handler to use the correct values that are fetched from the DB and is also using the new announcement method to talk to the welcome channel

--- src/Perpetuum/Services/Channels/ChannelMessageHandler.cs
+++ src/Perpetuum/Services/Channels/ChannelMessageHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Perpetuum.Accounting.Characters;
+using Perpetuum.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,13 +11,24 @@ namespace Perpetuum.Services.Channels
     public static class ChannelMessageHandler
     {
         private const string WELCOME_CHANNEL_NAME = "General chat";
-        private const string SENDER_CHARACTER_NICKNAME = "Placeholder";
+        private const string SENDER_CHARACTER_NICKNAME = "New Player Announcer";
 
         public static void SendWelcomeMessageExitTutorial(IChannelManager channelManager, string newCharacterName)
         {
-            string welcomeMessage = string.Format("Please welcome our new player: {0}", newCharacterName);
-            Character sender = Character.GetByNick(newCharacterName);
-            channelManager.Talk(WELCOME_CHANNEL_NAME, sender, welcomeMessage, null);
+            string message;
+            using (var scope = Db.CreateTransaction())
+            {
+                var records = Db.Query()
+                    .CommandText("SELECT message FROM premadechatmessage WHERE name = @messageName")
+                    .SetParameter("@messageName", "TutorialCompleteMessage")
+                    .Execute();
+                message = records.First().GetValue<string>("message");
+                scope.Complete();
+            }
+            message = message.Replace("$NAME$", newCharacterName);
+
+            Character sender = Character.GetByNick(SENDER_CHARACTER_NICKNAME);
+            channelManager.Announcement(WELCOME_CHANNEL_NAME, sender, message);
         }
     }
 }
