00235bcfe15ca1b4560422321ce63a7d3c1970d5
Philippe Cloutier <philippe.cloutier.1@gmail.com>
2019-05-11 16:09:22 -0400

    - Add message at the initial log-in of new players - Update message handler to support more messages easily

--- src/Perpetuum/Services/Channels/ChannelMessageHandler.cs
+++ src/Perpetuum/Services/Channels/ChannelMessageHandler.cs
@@ -10,17 +10,30 @@ namespace Perpetuum.Services.Channels
 {
     public static class ChannelMessageHandler
     {
-        private const string WELCOME_CHANNEL_NAME = "General chat";
+        private const string WELCOME_CHANNEL_NAME = "Recruitment";
+        private const string HELP_CHANNEL_NAME = "regchannel_help";
         private const string SENDER_CHARACTER_NICKNAME = "New Player Announcer";
 
+        public static void SendNewPlayerTutorialMessage(IChannelManager channelManager, string newCharacterName)
+        {
+            var test = channelManager.Channels.All(channel => channel.Name.ToLower().Contains("help"));
+
+            SendMessage(channelManager, HELP_CHANNEL_NAME, "NewPlayerEnteredTheGame", newCharacterName);
+        }
+
         public static void SendWelcomeMessageExitTutorial(IChannelManager channelManager, string newCharacterName)
+        {
+            SendMessage(channelManager, WELCOME_CHANNEL_NAME, "NewPlayerFinishedTheTutorial", newCharacterName);
+        }
+
+        private static void SendMessage(IChannelManager channelManager, string channelName, string messageName, string newCharacterName)
         {
             string message;
             using (var scope = Db.CreateTransaction())
             {
                 var records = Db.Query()
                     .CommandText("SELECT message FROM premadechatmessage WHERE name = @messageName")
-                    .SetParameter("@messageName", "TutorialCompleteMessage")
+                    .SetParameter("@messageName", messageName)
                     .Execute();
                 message = records.First().GetValue<string>("message");
                 scope.Complete();
@@ -28,7 +41,7 @@ namespace Perpetuum.Services.Channels
             message = message.Replace("$NAME$", newCharacterName);
 
             Character sender = Character.GetByNick(SENDER_CHARACTER_NICKNAME);
-            channelManager.Announcement(WELCOME_CHANNEL_NAME, sender, message);
+            channelManager.Announcement(channelName, sender, message);
         }
     }
 }
