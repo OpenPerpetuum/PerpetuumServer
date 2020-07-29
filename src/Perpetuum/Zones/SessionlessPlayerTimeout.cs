8c347ce7e45e0807f0842d31fa50955ee92b9077
mikejeffers <mike.s.jeffers@gmail.com>
2020-07-28 20:48:16 -0400

    linq overhaul

--- src/Perpetuum/Zones/SessionlessPlayerTimeout.cs
+++ src/Perpetuum/Zones/SessionlessPlayerTimeout.cs
@@ -7,17 +7,20 @@ using System.Linq;
 
 namespace Perpetuum.Zones
 {
+    /// <summary>
+    /// A class used by a Zone to determine if a Player is orphaned by a timeout check on its ZoneSession
+    /// </summary>
     public class SessionlessPlayerTimeout
     {
-        private static readonly TimeSpan MAX_ORPHAN_TIME = TimeSpan.FromMinutes(6);
-        private static readonly TimeSpan UPDATE_RATE = TimeSpan.FromMinutes(2);
+        private static readonly TimeSpan MAX_ORPHAN_TIME = TimeSpan.FromMinutes(2);
+        private static readonly TimeSpan UPDATE_RATE = TimeSpan.FromMinutes(1);
         private readonly IZone _zone;
-        private readonly IDictionary<long, PlayerTimeout> _orphanPlayers;
+        private IEnumerable<PlayerTimeout> _orphanPlayers;
 
         public SessionlessPlayerTimeout(IZone zone)
         {
             _zone = zone;
-            _orphanPlayers = new Dictionary<long, PlayerTimeout>();
+            _orphanPlayers = new HashSet<PlayerTimeout>();
         }
 
         private TimeSpan _elapsed = TimeSpan.Zero;
@@ -33,35 +36,28 @@ namespace Perpetuum.Zones
 
         private void DoUpdate()
         {
-            Logger.DebugInfo($"Zone:{_zone.Id} Total players: {_zone.Players.Count()} Total orphan players: {_zone.Players.Count(p => p.Session == ZoneSession.None)}");
-
-            // Players on the zone without a session - this is ok unless the session never connects!
-            var playersWithoutSessions = _zone.Players.Where(p => p.Session == ZoneSession.None)
-                .Where(p => !_orphanPlayers.Keys.Contains(p.Eid));
-
-            foreach (var p in playersWithoutSessions)
-            {
-                _orphanPlayers.Add(p.Eid, new PlayerTimeout(p));
-            }
-
-            // Players that are no longer orphaned
-            _orphanPlayers.RemoveRange(_orphanPlayers.Where(o => o.Value.Player.Session != ZoneSession.None).GetKeys().ToArray());
-
-            // Players with Expired timers; Zone assumes the Player has failed to connect and removes them
-            var toRemove = _orphanPlayers.Where(o => o.Value.Expired).ToArray();
-
-            foreach (var o in toRemove)
-            {
-                o.Value.Player.RemoveFromZone();
-                _orphanPlayers.Remove(o.Key);
-                Logger.DebugInfo($"A player w/o session REMOVED: {o.Value.Player}");
-            }
+            _orphanPlayers = AddSessionlessPlayers(_orphanPlayers, _zone);
+            _orphanPlayers = RemoveExpiredOrphansFromZone(_orphanPlayers);
         }
 
-        private class PlayerTimeout
+        private static IEnumerable<PlayerTimeout> AddSessionlessPlayers(IEnumerable<PlayerTimeout> orphans, IZone zone)
+        {
+            var sessionLessPlayers = zone.Players.Where(p => p.Session == ZoneSession.None).Select(p => new PlayerTimeout(p)).ToHashSet();
+            return orphans.Union(sessionLessPlayers).Where(o => o.Player.Session == ZoneSession.None).ToHashSet();
+        }
+
+        private static IEnumerable<PlayerTimeout> RemoveExpiredOrphansFromZone(IEnumerable<PlayerTimeout> orphans)
+        {
+            var toRemove = orphans.Where(o => o.Expired);
+            toRemove.ForEach(o => o.Player.RemoveFromZone());
+            return orphans.Except(toRemove).ToHashSet();
+        }
+
+        private class PlayerTimeout : IEquatable<PlayerTimeout>
         {
             public Player Player { get; private set; }
             public bool Expired { get { return _time.Expired; } }
+            public long Eid { get { return Player is null ? 0 : Player.Eid; } }
             private readonly TimeKeeper _time;
 
             public PlayerTimeout(Player p)
@@ -69,7 +65,32 @@ namespace Perpetuum.Zones
                 _time = new TimeKeeper(MAX_ORPHAN_TIME);
                 Player = p;
             }
-        }
 
+            public bool Equals(PlayerTimeout other)
+            {
+                if (other is null)
+                    return false;
+
+                return ReferenceEquals(this, other) || Eid == other.Eid;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj is null)
+                {
+                    return false;
+                }
+                else if (obj is PlayerTimeout pt)
+                {
+                    return Equals(pt);
+                }
+                return false;
+            }
+
+            public override int GetHashCode()
+            {
+                return Eid.GetHashCode();
+            }
+        }
     }
 }
