398decd657788b6e558c0b9df14794c54e342c55
mikejeffers <mike.s.jeffers@gmail.com>
2020-08-22 18:20:34 -0400

    unused import

--- src/Perpetuum/Zones/NpcSystem/ThreatManager.cs
+++ src/Perpetuum/Zones/NpcSystem/ThreatManager.cs
@@ -4,7 +4,6 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using Perpetuum.Log;
 using Perpetuum.Threading;
 using Perpetuum.Timers;
 using Perpetuum.Units;
