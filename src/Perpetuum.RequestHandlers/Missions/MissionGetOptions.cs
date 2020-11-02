d18c612fd461de333193695928c660fb0e528298
DerekJarvis <6828105+DerekJarvis@users.noreply.github.com>
2020-11-02 01:24:29 +0000

    First commit for rally terminals (field terminals with no assignments)

--- src/Perpetuum.RequestHandlers/Missions/MissionGetOptions.cs
+++ src/Perpetuum.RequestHandlers/Missions/MissionGetOptions.cs
@@ -1,6 +1,9 @@
-﻿using Perpetuum.Host.Requests;
+﻿using Perpetuum.EntityFramework;
+using Perpetuum.ExportedTypes;
+using Perpetuum.Host.Requests;
 using Perpetuum.Services.MissionEngine.MissionDataCacheObjects;
 using Perpetuum.Services.MissionEngine.MissionProcessorObjects;
+using Perpetuum.Units.FieldTerminals;
 
 namespace Perpetuum.RequestHandlers.Missions
 {
@@ -8,11 +11,13 @@ namespace Perpetuum.RequestHandlers.Missions
     {
         private readonly MissionProcessor _missionProcessor;
         private readonly MissionDataCache _missionDataCache;
+        private readonly IEntityRepository _entityRepository;
 
-        public MissionGetOptions(MissionProcessor missionProcessor,MissionDataCache missionDataCache)
+        public MissionGetOptions(MissionProcessor missionProcessor,MissionDataCache missionDataCache, IEntityRepository entityRepository)
         {
             _missionProcessor = missionProcessor;
             _missionDataCache = missionDataCache;
+            _entityRepository = entityRepository;
         }
 
         public void HandleRequest(IRequest request)
@@ -20,19 +25,31 @@ namespace Perpetuum.RequestHandlers.Missions
             var character = request.Session.Character;
             long locationEid;
 
+            FieldTerminal terminal = null;
+
             if (character.IsDocked)
             {
                 //the base we the player is docked
                 locationEid = character.CurrentDockingBaseEid;
+
             }
             else
             {
                 //use the optional parameter
                 locationEid = request.Data.GetOrDefault<long>(k.eid);
+
+                // Load Entity to see if it's a custom field terminal
+                terminal = (FieldTerminal)_entityRepository.Load(locationEid).ThrowIfNull(ErrorCodes.TargetNotFound);
             }
 
-            var location = _missionDataCache.GetLocationByEid(locationEid).ThrowIfNull(ErrorCodes.ItemNotFound);
-            _missionProcessor.GetOptionsByRequest(character, location);
+            // Don't show missions for rally terminals
+            if (terminal != null && terminal.ED.Name == DefinitionNames.FIELD_TERMINAL_RALLY) {
+                throw PerpetuumException.Create(ErrorCodes.MissionNotAvailable);
+            } else {
+                var location = _missionDataCache.GetLocationByEid(locationEid).ThrowIfNull(ErrorCodes.ItemNotFound);
+                _missionProcessor.GetOptionsByRequest(character, location);
+            }
+            
         }
     }
 }
