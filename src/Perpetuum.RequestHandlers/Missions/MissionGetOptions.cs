f500d3543e75bb57734bdeb73046ab6aeac623ef
mikejeffers <mike.s.jeffers@gmail.com>
2020-11-18 21:56:29 -0500

    refactor

--- src/Perpetuum.RequestHandlers/Missions/MissionGetOptions.cs
+++ src/Perpetuum.RequestHandlers/Missions/MissionGetOptions.cs
@@ -13,7 +13,7 @@ namespace Perpetuum.RequestHandlers.Missions
         private readonly MissionDataCache _missionDataCache;
         private readonly IEntityRepository _entityRepository;
 
-        public MissionGetOptions(MissionProcessor missionProcessor,MissionDataCache missionDataCache, IEntityRepository entityRepository)
+        public MissionGetOptions(MissionProcessor missionProcessor, MissionDataCache missionDataCache, IEntityRepository entityRepository)
         {
             _missionProcessor = missionProcessor;
             _missionDataCache = missionDataCache;
@@ -25,31 +25,24 @@ namespace Perpetuum.RequestHandlers.Missions
             var character = request.Session.Character;
             long locationEid;
 
-            FieldTerminal terminal = null;
-
             if (character.IsDocked)
             {
                 //the base we the player is docked
                 locationEid = character.CurrentDockingBaseEid;
-
             }
             else
             {
                 //use the optional parameter
                 locationEid = request.Data.GetOrDefault<long>(k.eid);
-
                 // Load Entity to see if it's a custom field terminal
-                terminal = (FieldTerminal)_entityRepository.Load(locationEid).ThrowIfNull(ErrorCodes.TargetNotFound);
+                var terminal = _entityRepository.Load(locationEid)
+                    .ThrowIfNull(ErrorCodes.TargetNotFound)
+                    .ThrowIfNotType<FieldTerminal>(ErrorCodes.MissionNotAvailable);
+                // Don't show missions for rally terminals
+                terminal.ED.Name.ThrowIfEqual(DefinitionNames.FIELD_TERMINAL_RALLY, ErrorCodes.MissionNotAvailable);
             }
-
-            // Don't show missions for rally terminals
-            if (terminal != null && terminal.ED.Name == DefinitionNames.FIELD_TERMINAL_RALLY) {
-                throw PerpetuumException.Create(ErrorCodes.MissionNotAvailable);
-            } else {
-                var location = _missionDataCache.GetLocationByEid(locationEid).ThrowIfNull(ErrorCodes.ItemNotFound);
-                _missionProcessor.GetOptionsByRequest(character, location);
-            }
-            
+            var location = _missionDataCache.GetLocationByEid(locationEid).ThrowIfNull(ErrorCodes.ItemNotFound);
+            _missionProcessor.GetOptionsByRequest(character, location);
         }
     }
 }
