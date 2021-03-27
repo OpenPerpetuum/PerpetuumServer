5815f00ad375e70b09ad8013543218229e29bc6a
Veritania <Jesper_ant@hotmail.com>
2021-03-27 23:00:12 +0100

    Validate Args and split logic to new class - zoneAddDecor

---
+++ src/Perpetuum/Services/Channels/ChatCommands/AdminCommandLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Perpetuum.GenXY;
+
+namespace Perpetuum.Services.Channels.ChatCommands
+{
+    public class AdminCommandLogic
+    {
+        public static string ZoneAddDecor(int? zoneId, int definition, int x, int y, int z, double qx, double qy, double qz, double qw, double scale, int cat)
+        {
+            Dictionary<string, object> dictionary = new Dictionary<string, object>()
+                {
+                    { "definition", definition },
+                    { "x", x*256 },
+                    { "y", y*256 },
+                    { "z", z*256 },
+                    { "quaternionX", qx },
+                    { "quaternionY", qy },
+                    { "quaternionZ", qz },
+                    { "quaternionW", qw },
+                    { "scale", scale },
+                    { "category", cat }
+                };
+
+            return string.Format("zoneDecorAdd:zone_{0}:{1}", zoneId, GenxyConverter.Serialize(dictionary));
+        }
+    }
+}
