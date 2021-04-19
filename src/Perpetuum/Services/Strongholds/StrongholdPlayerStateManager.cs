0aaf5b02814e795cc9b952db162e9cf63cb00376
mikejeffers <mike.s.jeffers@gmail.com>
2021-04-19 01:30:06 -0400

    track logged off characters to monitor their logged time, do removal if over limit, extract some logic outside of transaction scope

--- src/Perpetuum/Services/Strongholds/StrongholdPlayerStateManager.cs
+++ src/Perpetuum/Services/Strongholds/StrongholdPlayerStateManager.cs
@@ -5,7 +5,9 @@ using Perpetuum.Threading.Process;
 using Perpetuum.Timers;
 using Perpetuum.Zones;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Transactions;
 
 namespace Perpetuum.Services.Strongholds
 {
@@ -13,9 +15,14 @@ namespace Perpetuum.Services.Strongholds
 
     public class StrongholdPlayerStateManager : Process, IStrongholdPlayerStateManager
     {
+        private readonly TimeSpan MAX_IDLE_TIME = TimeSpan.FromMinutes(1.5); //TODO: test value, TBD time period of no client activity
+        private readonly TimeSpan MAX_DISCONNECT_TIME = TimeSpan.FromMinutes(2); //TODO: test value, TBD time period of logged off character
         private readonly IZone _zone;
         private readonly ISessionManager _sessionManager;
         private readonly TimeTracker _updateTimer = new TimeTracker(TimeSpan.FromSeconds(31));
+
+        private IDictionary<int, TimeTracker> _loggedOffCharacters = new Dictionary<int, TimeTracker>();
+
         public StrongholdPlayerStateManager(IZone zone, ISessionManager sessionManager)
         {
             _zone = zone;
@@ -27,40 +34,66 @@ namespace Perpetuum.Services.Strongholds
             _updateTimer.Update(time);
             if (_updateTimer.Expired)
             {
-                DoCheck();
+                DoCheck(time);
                 _updateTimer.Reset();
             }
         }
 
-        public void DoCheck()
+        public void DoCheck(TimeSpan time)
         {
-            DockUpOfflinePlayers();
-            var characters = _zone.Players.Where(p => p.Session.InactiveTime > TimeSpan.FromMinutes(3)).Select(p => p.Character);
-            foreach (var character in characters)
-            {
-                DockPlayer(character);
-            }
+            DockUpOfflinePlayers(time);
+            DockOnlinePlayers();
         }
 
-        private void DockUpOfflinePlayers()
+        private void DockUpOfflinePlayers(TimeSpan time)
         {
+            IEnumerable<int> offlineCharIds;
             using (var scope = Db.CreateTransaction())
             {
                 // Query for all offline chars on zone
-                var offlineCharIds = Db.Query().CommandText(
+                offlineCharIds = Db.Query().CommandText(
                     $"SELECT characterID FROM characters WHERE inUse=0 AND zoneID=@zoneId;")
                     .SetParameter("@zoneId", _zone.Id)
                     .Execute()
                     .Select(r => r.GetValue<int>("characterID"));
 
-                // Set DB state of characters to be docked at homebase
                 foreach (var charId in offlineCharIds)
                 {
-                    Db.Query().CommandText("opp.characterForceDockHome")
-                                        .SetParameter("@characterId", charId)
-                                        .ExecuteNonQuery();
+                    // Track how long character is logged off for
+                    if (_loggedOffCharacters.ContainsKey(charId))
+                    {
+                        _loggedOffCharacters[charId].Update(time);
+                    }
+                    else
+                    {
+                        _loggedOffCharacters[charId] = new TimeTracker(MAX_DISCONNECT_TIME);
+                    }
+
+                    // Set DB state of characters to be docked at homebase
+                    if (_loggedOffCharacters[charId].Expired)
+                    {
+                        Db.Query().CommandText("opp.characterForceDockHome")
+                            .SetParameter("@characterId", charId)
+                            .ExecuteNonQuery();
+                        // Remove character id from tracking
+                        _loggedOffCharacters.Remove(charId);
+                    }
                 }
                 scope.Complete();
+            }
+            // Remove any ids from tracking that are no longer in the current logged off set
+            foreach (var key in _loggedOffCharacters.Keys.Where(k => !offlineCharIds.Contains(k)))
+            {
+                _loggedOffCharacters.Remove(key);
+            }
+        }
+
+        private void DockOnlinePlayers()
+        {
+            var characters = _zone.Players.Where(p => p.Session.InactiveTime > MAX_IDLE_TIME).Select(p => p.Character);
+            foreach (var character in characters)
+            {
+                DockPlayer(character);
             }
         }
 
@@ -72,9 +105,12 @@ namespace Perpetuum.Services.Strongholds
                 var dockingBase = character.GetHomeBaseOrCurrentBase();
                 dockingBase.DockIn(character, TimeSpan.FromSeconds(5), ZoneExitType.Docked);
 
-                // Send update to client if online
-                var session = _sessionManager.GetByCharacter(character);
-                Message.Builder.ToClient(session).WithError(ErrorCodes.YouAreHappyNow).Send();
+                Transaction.Current.OnCommited(() =>
+                {
+                    // Send update to client if online
+                    var session = _sessionManager.GetByCharacter(character);
+                    Message.Builder.ToClient(session).WithError(ErrorCodes.YouAreHappyNow).Send();
+                });
 
                 scope.Complete();
             }
