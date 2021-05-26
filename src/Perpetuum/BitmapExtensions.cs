a4d2a7ffafb9aa5541fb5696c083aac06d6d5b7d
mikejeffers <mike.s.jeffers@gmail.com>
2021-05-26 00:35:50 -0400

    bitmap erode/dilate

--- src/Perpetuum/BitmapExtensions.cs
+++ src/Perpetuum/BitmapExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace Perpetuum
 {
@@ -37,6 +39,70 @@ namespace Perpetuum
             }
 
             return bitmap;
+        }
+
+
+        public static Bitmap DilateOrErode(this Bitmap sourceBitmap, int matrixSize, bool isErode)
+        {
+            var sourceData = sourceBitmap.LockBits(
+                new Rectangle(0, 0, sourceBitmap.Width, sourceBitmap.Height),
+                ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+            byte[] pixelBuffer = new byte[sourceData.Stride * sourceData.Height];
+            byte[] resultBuffer = new byte[sourceData.Stride * sourceData.Height];
+
+            Marshal.Copy(sourceData.Scan0, pixelBuffer, 0, pixelBuffer.Length);
+
+            sourceBitmap.UnlockBits(sourceData);
+
+            var filterOffset = (matrixSize - 1) / 2;
+            var morphResetValue = (byte)(isErode ? 0 : 1);
+
+            for (var offsetY = filterOffset; offsetY < sourceBitmap.Height - filterOffset; offsetY++)
+            {
+                for (var offsetX = filterOffset; offsetX < sourceBitmap.Width - filterOffset; offsetX++)
+                {
+                    var byteOffset = offsetY * sourceData.Stride + offsetX * 4;
+                    var value = morphResetValue;
+
+                    for (var filterY = -filterOffset; filterY <= filterOffset; filterY++)
+                    {
+                        for (var filterX = -filterOffset; filterX <= filterOffset; filterX++)
+                        {
+                            var calcOffset = byteOffset + (filterX * 4) + (filterY * sourceData.Stride);
+                            if (isErode)
+                            {
+                                if (pixelBuffer[calcOffset] < value)
+                                {
+                                    value = pixelBuffer[calcOffset];
+                                }
+                            }
+                            else
+                            {
+                                if (pixelBuffer[calcOffset] > value)
+                                {
+                                    value = pixelBuffer[calcOffset];
+                                }
+                            }
+
+                        }
+                    }
+                    value = pixelBuffer[byteOffset];
+                    resultBuffer[byteOffset] = value;
+                }
+            }
+
+            var resultBitmap = new Bitmap(sourceBitmap.Width, sourceBitmap.Height);
+
+            var resultData = resultBitmap.LockBits(
+                new Rectangle(0, 0, resultBitmap.Width, resultBitmap.Height),
+                ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0, resultBuffer.Length);
+
+            resultBitmap.UnlockBits(resultData);
+
+            return resultBitmap;
         }
     }
 }
