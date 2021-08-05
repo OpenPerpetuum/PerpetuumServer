75e39a5b3e3484d5f30f3aebde309c211cbac922
mikejeffers <mike.s.jeffers@gmail.com>
2021-08-04 21:22:08 -0400

    populate groundtype using fuzzy circles

--- src/Perpetuum.Bootstrapper/PerpetuumBootstrapper.cs
+++ src/Perpetuum.Bootstrapper/PerpetuumBootstrapper.cs
@@ -2773,6 +2773,7 @@ namespace Perpetuum.Bootstrapper
             RegisterZoneRequestHandler<ZoneDrawBlockingByDefinition>(Commands.ZoneDrawBlockingByDefinition);
             RegisterZoneRequestHandler<ZoneCleanBlockingByDefinition>(Commands.ZoneCleanBlockingByDefinition);
             RegisterZoneRequestHandler<ZoneCleanObstacleBlocking>(Commands.ZoneCleanObstacleBlocking);
+            RegisterZoneRequestHandler<ZoneFillGroundTypeRandom>(Commands.ZoneFillGroundTypeRandom);
 
 
 
