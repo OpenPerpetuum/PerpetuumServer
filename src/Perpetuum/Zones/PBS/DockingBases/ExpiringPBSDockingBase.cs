affb60b69bd32853e49937ec8095b6cee4453fdc
mikejeffers <mike.s.jeffers@gmail.com>
2021-10-10 03:26:32 -0400

    expiring pbs base, fix bug where system storage node was getting deleted twice on pbs death

---
+++ src/Perpetuum/Zones/PBS/DockingBases/ExpiringPBSDockingBase.cs
@@ -0,0 +1,84 @@
+﻿using Perpetuum.Common;
+using Perpetuum.Groups.Corporations;
+using Perpetuum.Items.Templates;
+using Perpetuum.Services.Channels;
+using Perpetuum.Services.MarketEngine;
+using Perpetuum.Services.Sparks.Teleports;
+using Perpetuum.Units;
+using Perpetuum.Units.DockingBases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perpetuum.Zones.PBS.DockingBases
+{
+    public class ExpiringPBSDockingBase : PBSDockingBase
+    {
+        private IUnitDespawnHelper _despawnHelper;
+        private TimeSpan _aliveElapsed;
+        private readonly TimeSpan _alertPeriod = TimeSpan.FromMinutes(1);
+        private bool _alerted;
+
+        public ExpiringPBSDockingBase(
+            MarketHelper marketHelper,
+            ICorporationManager corporationManager,
+            IChannelManager channelManager,
+            ICentralBank centralBank,
+            IRobotTemplateRelations robotTemplateRelations,
+            DockingBaseHelper dockingBaseHelper,
+            SparkTeleportHelper sparkTeleportHelper,
+            PBSObjectHelper<PBSDockingBase>.Factory pbsObjectHelperFactory) : base(marketHelper,
+             corporationManager,
+             channelManager,
+             centralBank,
+             robotTemplateRelations,
+             dockingBaseHelper,
+             sparkTeleportHelper,
+             pbsObjectHelperFactory)
+        {
+        }
+
+        public TimeSpan LifeTime
+        {
+            get
+            {
+                return TimeSpan.FromSeconds(ED.Config.lifeTime ?? (60 * 5));
+            }
+        }
+
+        protected override void OnEnterZone(IZone zone, ZoneEnterType enterType)
+        {
+            _despawnHelper = UnitDespawnHelper.Create(this, LifeTime);
+            _despawnHelper.DespawnStrategy = Kill;
+
+            base.OnEnterZone(zone, enterType);
+        }
+
+        protected override void OnUpdate(TimeSpan time)
+        {
+            if (!_alerted)
+                WarnIfAboutToExpire(time);
+
+            _despawnHelper?.Update(time, this);
+
+            base.OnUpdate(time);
+        }
+
+        private void WarnIfAboutToExpire(TimeSpan time)
+        {
+            _aliveElapsed += time;
+            if (LifeTime - _aliveElapsed < _alertPeriod)
+            {
+                Task.Run(() => _pbsObjectHelper.SendAttackAlert());
+                _alerted = true;
+            }
+        }
+
+        public override ErrorCodes IsDeconstructAllowed()
+        {
+            return ErrorCodes.DockingBaseNotSetToDeconstruct;
+        }
+    }
+}
